94c7e9edbf652ca2bac0abdc1d80326e5ad1fca2
Keith Smyth <keith.smyth@outware.com.au>
2015-02-22 20:55:33 +1100

    Move fragment hides category if null, improved loading screen

--- app/src/main/java/com/keithsmyth/pokemans/ui/MoveFragment.java
+++ app/src/main/java/com/keithsmyth/pokemans/ui/MoveFragment.java
@@ -6,11 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.keithsmyth.pokemans.App;
 import com.keithsmyth.pokemans.R;
-import com.keithsmyth.pokemans.data.Callback;
 import com.keithsmyth.pokemans.model.Move;
 
 /**
@@ -30,20 +28,14 @@ public class MoveFragment extends BaseDataFragment<Move> {
 
   private TextView nameText;
   private TextView descriptionText;
-  private TextView categoryText;
-  private TextView powerText;
-  private TextView ppText;
-  private TextView accuracyText;
+  private ViewGroup itemsLayout;
 
   @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                                      @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_move, container, false);
     nameText = (TextView) view.findViewById(R.id.txt_name);
     descriptionText = (TextView) view.findViewById(R.id.txt_description);
-    categoryText = (TextView) view.findViewById(R.id.txt_category);
-    powerText = (TextView) view.findViewById(R.id.txt_power);
-    ppText = (TextView) view.findViewById(R.id.txt_pp);
-    accuracyText = (TextView) view.findViewById(R.id.txt_accuracy);
+    itemsLayout = (ViewGroup) view.findViewById(R.id.layout_items);
     return view;
   }
 
@@ -60,9 +52,22 @@ public class MoveFragment extends BaseDataFragment<Move> {
     if (getView() == null) return;
     nameText.setText(move.name);
     descriptionText.setText(move.description);
-    categoryText.setText(move.category);
-    powerText.setText(String.valueOf(move.power));
-    ppText.setText(String.valueOf(move.pp));
-    accuracyText.setText(String.valueOf(move.accuracy));
+
+    itemsLayout.removeAllViews();
+    LayoutInflater inflater = LayoutInflater.from(getView().getContext());
+    populateItem(inflater, itemsLayout, getString(R.string.move_category), move.category);
+    populateItem(inflater, itemsLayout, getString(R.string.move_power), String.valueOf(move.power));
+    populateItem(inflater, itemsLayout, getString(R.string.move_pp), String.valueOf(move.pp));
+    populateItem(inflater, itemsLayout, getString(R.string.move_accuracy),
+        String.valueOf(move.accuracy));
+  }
+
+  private void populateItem(LayoutInflater inflater, ViewGroup viewGroup, String title,
+                            String value) {
+    if (value == null) return;
+    View view = inflater.inflate(R.layout.move_detail_item, viewGroup, false);
+    ((TextView) view.findViewById(R.id.txt_title)).setText(title);
+    ((TextView) view.findViewById(R.id.txt_value)).setText(value);
+    viewGroup.addView(view);
   }
 }
